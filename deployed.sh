616d2d77926d6be24da055406ca95316a91838f6
15166089918 <15166089918@163.com>
2020-04-13 13:25:00 +0800

    代码自动化部署脚本文件

--- deployed.sh
+++ deployed.sh
@@ -1,13 +1,21 @@
- #!/bin/bash
+#!/bin/bash
 W4J_PATH='/home/ihuzb/'$1
+W4J_HOT="W4JHotComments"
+W4J_DATA="W4JDataService"
 
 echo "***开始部署***"
 cd $W4J_PATH
 git pull origin master
 echo "***更新结束***"
-if ($1=="W4JHotComments");then
-    NODE_LOG_DIR=/tmp ENABLE_NODE_LOG=YES pm2 reload index.js --name W4J
+if [ "$1"="$W4J_HOT" ];then
+    NODE_LOG_DIR=/tmp ENABLE_NODE_LOG=YES pm2 reload index.js
 else
-    NODE_LOG_DIR=/tmp ENABLE_NODE_LOG=YES pm2 reload ./bin/www --name W4J
+    echo "$W4J_DATA"
+fi
+
+if [ "$1"="$W4J_DATA" ];then
+    NODE_LOG_DIR=/tmp ENABLE_NODE_LOG=YES pm2 reload ./bin/www
+else
+    echo "$W4J_HOT"
 fi
 echo "***部署结束***"
